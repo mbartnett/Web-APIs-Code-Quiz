3611146fda3621d5bf5185eaa088f5b82f3f5225
Mike B <115735557+mbartnett@users.noreply.github.com>
2022-11-11 14:28:51 -0600

    further syntax edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,56 +4,56 @@ let questions = [
     {
         number: 1,
         question: "Commonly used data types DO NOT include",
-        answer: "3. alerts",
         options: [
             "1. strings",
             "2. booleans",
             "3. alerts",
             "4. numbers"
-        ]
+        ],
+        answer: "3. alerts"
     },
     {
         number: 2,
         question: "The condition in an if / else statement is enclosed with _______.",
-        answer: "3. parenthesis",
         options: [
             "1. quotes",
             "2. curly brackets",
             "3. parenthesis",
             "4. square brackets"
-        ]
+        ],
+        answer: "3. parenthesis",
 },  
 {
     number: 3,
     question: "Arrays in Javascript can be used to store _______.",
-    answer: "4. all of the above",
     options: [
         "1. numbers and strings",
         "2. other arrays",
         "3. booleans",
         "4. all of the above"
-        ]
+        ],
+    answer: "4. all of the above"
     },
     {
     number: 4,
     question: "String values must be enclosed within _______ when being assigned to variables.",
-    answer: "3. quotes",
     options: [
         "1. commas",
         "2. curly brackets",
         "3. quotes",
         "4. parenthesis",
-        ]
+        ],
+    answer: "3. quotes"
     }, 
     {
     number: 5,
     question: "A very useful tool used during development and debugging is:",
-    answer: "4. console log",
     options: [
         "1. Javascript",
         "2. terminal/bash", 
         "3. for loops",
         "4. console log",
-        ]
+        ],
+    answer: "4. console log"
     }
 ]
