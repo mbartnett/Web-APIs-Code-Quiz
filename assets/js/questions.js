6b791bcf4b586aa037e4c24dc8d79bd9b4e55f85
Mike B <115735557+mbartnett@users.noreply.github.com>
2022-11-15 14:55:29 -0600

    struggling but small edits

---
+++ assets/js/questions.js
@@ -0,0 +1,54 @@
+// An array of objects containing the number of the quesiton, the quesiton, the possible solutions and the answers.
+
+let questions = [
+    {
+        question: "Commonly used data types DO NOT include",
+        choices: [
+            "strings",
+            "booleans",
+            "alerts",
+            "numbers"
+        ],
+        answer: "alerts"
+    },
+    {
+        question: "The condition in an if / else statement is enclosed with _______.",
+        choices: [
+            "quotes",
+            "curly brackets",
+            "parenthesis",
+            "square brackets"
+        ],
+        answer: "parenthesis",
+    },  
+    {
+        question: "Arrays in Javascript can be used to store _______.",
+        choices: [
+            "numbers and strings",
+            "other arrays",
+            "booleans",
+            "all of the above"
+        ],
+        answer: "all of the above"
+    },
+    {
+        question: "String values must be enclosed within _______ when being assigned to variables.",
+        choices: [
+            "commas",
+            "curly brackets",
+            "quotes",
+            "parenthesis",
+        ],
+        answer: "quotes"
+    }, 
+    {
+        question: "A very useful tool used during development and debugging is:",
+        choices: [
+            "Javascript",
+            "terminal/bash", 
+            "for loops",
+            "console log",
+        ],
+    answer: "console log"
+    }
+];
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,90 +1,12 @@
-// General variables
-var question = document.querySelector('#question');
-var options = document.querySelector('#options');
-var timerText = document.querySelector('#timer');
+// Variables
 
-var startButton = document.querySelector(".start-button");
-var startTime = 60;
-var question = 0;
+var startButton = document.querySelector("#start-button");
+startButton.addEventListener("click", startTimer);
 
-// Function to start the timer
-
-function startTimer() {
-    remainingTimeEl.textContent = timeRemaining;
-    interval = setInterval(function () {
-        timeRemaining--;
-        remainingTimeEl.textContent = timeRemaining;
-
-        if (timeRemaining === 0){
-            clearInterval(interval);
-            cardEl.textContent = "Time is up!"
-        }
-    }, 1000)
-}
-startTimer ()
-
-// Function to stop the timer
-
-function stopTimer(){
-    clearInterval(interval)
+startTimer () {
+    ;
 }
 
-// An array of objects containing the number of the quesiton, the quesiton, the possible solutions and the answers.
-
-let questions = [
-    {
-        number: 1,
-        question: "Commonly used data types DO NOT include",
-        options: [
-            "1. strings",
-            "2. booleans",
-            "3. alerts",
-            "4. numbers"
-        ],
-        answer: "3. alerts"
-    },
-    {
-        number: 2,
-        question: "The condition in an if / else statement is enclosed with _______.",
-        options: [
-            "1. quotes",
-            "2. curly brackets",
-            "3. parenthesis",
-            "4. square brackets"
-        ],
-        answer: "3. parenthesis",
-},  
-{
-    number: 3,
-    question: "Arrays in Javascript can be used to store _______.",
-    options: [
-        "1. numbers and strings",
-        "2. other arrays",
-        "3. booleans",
-        "4. all of the above"
-        ],
-    answer: "4. all of the above"
-    },
-    {
-    number: 4,
-    question: "String values must be enclosed within _______ when being assigned to variables.",
-    options: [
-        "1. commas",
-        "2. curly brackets",
-        "3. quotes",
-        "4. parenthesis",
-        ],
-    answer: "3. quotes"
-    }, 
-    {
-    number: 5,
-    question: "A very useful tool used during development and debugging is:",
-    options: [
-        "1. Javascript",
-        "2. terminal/bash", 
-        "3. for loops",
-        "4. console log",
-        ],
-    answer: "4. console log"
-    }
-]
+var time
+var questions
+var choices 
